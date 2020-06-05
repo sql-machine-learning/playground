53f8ef58889019454fb23773f5dce47810586067
Yi Wang <yi.wang.2005@gmail.com>
2020-06-04 17:11:16 -0700

    Rename

--- play.sh
+++ play.sh
@@ -15,34 +15,40 @@
 
 set -e
 
-echo
-echo "Welcome to SQLFlow playground!"
-echo
+cat <<EOF
+
+Welcome to SQLFlow playground!
+
+EOF
 
 if ! which vagrant >/dev/null; then
-  echo "Install vagrant first, please refer to https://www.vagrantup.com/downloads.html"
-  echo
-  echo "If you are using macOS, the operation system may prevent you from installing the web-downloaded package, you can try use brew to install vagrant. like:"
-  echo "brew cask install vagrant"
-  echo
-  echo "Fix this and re-run this script again please!"
-  exit 0
+    cat <<EOF
+We need Vagrant to run the playground.
+
+Linux users can refer to https://www.vagrantup.com/downloads.html for the
+installation guide.
+
+macOS users can install Vagrant using Homebrew:
+  brew cask install vagrant
+EOF
+    exit 1
 fi
 
 if [[ -n "$(vagrant global-status --prune | grep -o 'playground')" ]]; then
-  echo "It seems you have already installed our playground, exiting..."
-  exit 0
+    echo "The playground VM is running."
+    exit 0
 fi
 
-echo "Installing vagrant disk size plugin..."
 if [[ -z "$(vagrant plugin list | grep -o 'vagrant-disksize')" ]]; then
+    echo "Install Vagrant disk size plugin ..."
     vagrant plugin install vagrant-disksize
 fi
 
 CACHED_BOX="$HOME/.cache/sqlflow/ubuntu-bionic64.box"
 if [[ -f $CACHED_BOX ]]; then
+    echo "Found and use cached box $CACHED_BOX"
     vagrant box add ubuntu/bionic64 $CACHED_BOX
 fi
 
-echo "Start and provision the playgound now..."
+echo "Start and provision the playgound VM ..."
 vagrant up
